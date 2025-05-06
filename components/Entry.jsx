a9f611c980320aeccbe8b03379713054064cffea
Alin <alinalin03030@gmail.com>
2025-05-06 16:55:45 +0000

    aue

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -1,7 +1,19 @@
 export default function Entry(props){
     return (
         <article className="journal-entry">
-            
+            <div className="main-image-container">
+                <img 
+                    className="main-image"
+                    src={props.img.src}
+                    alt={props.img.alt} 
+                />
+            </div>
+            <div className="info-container">
+                <img 
+                    className="marker"
+                    src="../images/marker.png"
+                />
+            </div>
         </article>
     )
 }
